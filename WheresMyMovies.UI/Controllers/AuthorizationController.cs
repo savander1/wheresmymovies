fe3491d95685f0001b044fd0140ce3cfebe2db19
Shane Vander Meer <me@shanevandermeer.com>
2015-02-02 08:58:46 -0800

    Auth

--- WheresMyMovies.UI/Controllers/AuthorizationController.cs
+++ WheresMyMovies.UI/Controllers/AuthorizationController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WheresMyMovies.Models;
 
 namespace WheresMyMovies.Controllers
 {
@@ -11,6 +12,13 @@ namespace WheresMyMovies.Controllers
         // GET: Authorization
         public ActionResult Login()
         {
+            return View();
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public ActionResult Login(LoginModel loginModel)
+        {
+            
             return View();
         }
     }
