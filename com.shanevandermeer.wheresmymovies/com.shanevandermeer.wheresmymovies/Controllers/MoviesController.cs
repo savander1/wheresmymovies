02ab547cb021250c4e020b4fd32be0706abcf04c
unknown <me@shanevandermeer.com>
2015-08-04 21:47:06 -0700

    Adding repos, etc

--- com.shanevandermeer.wheresmymovies/com.shanevandermeer.wheresmymovies/Controllers/MoviesController.cs
+++ com.shanevandermeer.wheresmymovies/com.shanevandermeer.wheresmymovies/Controllers/MoviesController.cs
@@ -1,40 +1,47 @@
 ﻿using com.shanevandermeer.wheresmymovies.Models;
-using System;
+using com.shanevandermeer.wheresmymovies.Repository;
 using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Web.Http;
 
 namespace com.shanevandermeer.wheresmymovies.Controllers
 {
     public class MoviesController : ApiController
     {
+        private IRepository<Movie> _repository;
+
+        public MoviesController(IRepository<Movie> repository)
+        {
+            _repository = repository;
+        }
+
         // GET: api/Movie
         public IEnumerable<Movie> Get()
         {
-            return new Movie[] {  };
+            return _repository.GetAll();
         }
 
         // GET: api/Movie/5
-        public string Get(int id)
+        public Movie Get(string id)
         {
-            return "value";
+            return _repository.Get(id);
         }
 
         // POST: api/Movie
         public void Post([FromBody]Movie value)
         {
+            _repository.Insert(value);
         }
 
         // PUT: api/Movie/5
-        public void Put(int id, [FromBody]string value)
+        public void Put(string id, [FromBody]Movie value)
         {
+            _repository.Update(id, value);
         }
 
         // DELETE: api/Movie/5
-        public void Delete(int id)
+        public void Delete(string id)
         {
+            _repository.Delete(id);
         }
     }
 }
---
+++ com.shanevandermeer.wheresmymovies/com.shanevandermeer.wheresmymovies/Repository/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace com.shanevandermeer.wheresmymovies.Repository
+{
+    public interface IRepository<T>
+    {
+        IEnumerable<T> GetAll();
+        T Get(string Id);
+        bool Update(string id, T t);
+        bool Insert(T t);
+        bool Delete(string id);
+    }
+}
