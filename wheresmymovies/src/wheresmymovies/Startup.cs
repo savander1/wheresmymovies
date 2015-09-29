84d97d40b53d0f82feebec67ef77aa4fe5a72012
Shane Vander Meer <shane.vander.meer@globalrelay.net>
2015-09-29 13:22:27 -0700

    Getting webapi to run

--- wheresmymovies/src/wheresmymovies/Startup.cs
+++ wheresmymovies/src/wheresmymovies/Startup.cs
@@ -7,27 +7,22 @@ namespace wheresmymovies
 {
     public class Startup
     {
+    	public Startup(IHostingEnvironment env)
+	{
+	    
+	}
+
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddInstance<IMovieRepository>(new MovieRepository());
             services.AddMvc();
-            services.BuildServiceProvider();
         }
         
         public void Configure(IApplicationBuilder app)
         {
-            app.RunIISPipeline();
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    name: "default",
-                    template: "/api/{controller}/{action}");
-            });
-            app.Run(async (context) =>
-            { 
-                await context.Response.WriteAsync("Hello World!");
-            });   
+            app.UseStaticFiles();
+            app.UseMvc();   
         }
     }
 }
