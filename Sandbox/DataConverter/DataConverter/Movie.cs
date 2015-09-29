9625d36d1c526bd63629c07d47d73b8281e0c5d2
Shane Vander Meer <shane.vander.meer@globalrelay.net>
2015-09-29 11:21:21 -0700

    More Sandbox

---
+++ Sandbox/DataConverter/DataConverter/Movie.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace wheresmymovies.Entities
+{
+    public class MovieTEMP
+    {
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public string Rated { get; set; }
+        public string Released { get; set; }
+        public string Runtime { get; set; }
+        public string Genre { get; set; }
+        public string Director { get; set; }
+        public string Writer { get; set; }
+        public string Actors { get; set; }
+        public string Plot { get; set; }
+        public string Language { get; set; }
+        public string Country { get; set; }
+        public string Awards { get; set; }
+        public string Poster { get; set; }
+        public int Metascore { get; set; }
+        public decimal imdbRating { get; set; }
+        public long imdbVotes { get; set; }
+        public string imdbId { get; set; }
+        public string type { get; set; }
+        public bool Response { get; set; }
+    }
+
+    public class Movie
+    {
+        public string Id {get;set;}
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public string Rated { get; set; }
+        public DateTime Released { get; set; }
+        public TimeSpan Runtime { get; set; }
+        public List<string> Genre { get; set; }
+        public List<string> Director { get; set; }
+        public List<string> Writer { get; set; }
+        public List<string> Actors { get; set; }
+        public string Plot { get; set; }
+        public List<string> Language { get; set; }
+        public string Country { get; set; }
+        public string ThumbImgUrl { get; set; }
+        public string FullImgUrl { get; set; }
+
+        public Movie ToMovie(MovieTEMP temp)
+        {
+            return new Movie
+            {
+                Id = temp.imdbId,
+                Title = temp.Title,
+                Year = temp.Year,
+                Rated = temp.Rated,
+                Released = DateTime.Parse(temp.Released),
+                Runtime = TimeSpan.Parse(temp.Runtime),
+                Genre = temp.Genre.Split(",".ToCharArray()).ToList(),
+                Director = temp.Director.Split(",".ToCharArray()).ToList(),
+                Writer = temp.Writer.Split(",".ToCharArray()).ToList(),
+                Actors = temp.Actors.Split(",".ToCharArray()).ToList(),
+                Plot = temp.Plot,
+                Language = temp.Language.Split(",".ToCharArray()).ToList(),
+                Country = temp.Country,
+                ThumbImgUrl = GetThumbImageUrl(temp.Poster),
+                FullImgUrl = temp.Poster
+            };
+        }
+
+        private string GetThumbImageUrl(string poster)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Sandbox/DataConverter/DataConverter/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using wheresmymovies.Entities;
+
+namespace DataConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        } 
+
+        static IEnumerable<MovieTEMP> GetMovies()
+        {
+            var moviesString = ReadFileAsync().Result;
+
+            var movies = JsonConvert.DeserializeObject<IEnumerable<MovieTEMP>>(moviesString);
+
+            return movies.Select(x=> x.).Distinct();
+        } 
+
+        static Task<string> ReadFileAsync()
+        {
+            using (var stream = File.Open("", FileMode.Open))
+            {
+                using (var reader = new StreamReader(stream))
+                {
+                    return reader.ReadToEndAsync();
+                }
+            }
+        } 
+    }
+}
