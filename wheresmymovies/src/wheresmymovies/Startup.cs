0b2219ac71ccd7ea6f575ee931344869f83a6f1f
Shane Vander Meer <shane.vander.meer@globalrelay.net>
2015-10-02 12:46:33 -0700

    moving bower dependancies

--- wheresmymovies/src/wheresmymovies/Startup.cs
+++ wheresmymovies/src/wheresmymovies/Startup.cs
@@ -9,19 +9,19 @@ namespace wheresmymovies
     {
     	public Startup(IHostingEnvironment env)
 	    {
+            
 	    }
 
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddInstance<IMovieRepository>(new MovieRepository());
-            services.AddMvc();
         }
         
         public void Configure(IApplicationBuilder  app)
         {
-            app.UseStaticFiles();
             app.UseDefaultFiles();
+            app.UseStaticFiles();
             app.UseMvc();  
         }
     }
