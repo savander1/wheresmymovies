daf07299c06dde78d94dd38ccba9f1503e65cb54
unknown <me@shanevandermeer.com>
2015-10-31 15:04:42 -0700

    Connecting with Azure

--- wheresmymovies/src/wheresmymovies/Data/AzureSearchClient.cs
+++ wheresmymovies/src/wheresmymovies/Data/AzureSearchClient.cs
@@ -1,16 +1,44 @@
-﻿using System;
+﻿using Microsoft.Framework.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Net.Http;
 using System.Threading.Tasks;
+using wheresmymovies.Entities;
 
 namespace wheresmymovies.Data
 {
     public class AzureSearchClient
     {
+        private static readonly string SearchUrl;
+
+        static AzureSearchClient()
+        {
+            var urlFormatter = Startup.Configuration.Get<string>("Data:search:searchUrl");
+            var index = Startup.Configuration.Get<string>("Data:search:indexName");
+            var apiVersion = Startup.Configuration.Get<string>("Data:search:apiVersion");
+            SearchUrl = string.Format(urlFormatter, index, apiVersion);
+        }
+
     	public AzureSearchClient()
-	    {
-	         
-	    }
+        {
+
+        }
+
+        public async Task<bool> Add(Movie movie)
+        {
+            using (var client = new HttpClient())
+            {
+                var response = await client.PostAsync(SearchUrl, GetHttpContent(movie));
+                return response.IsSuccessStatusCode;
+            }
+        }
+
+        private HttpContent GetHttpContent(Movie movie)
+        {
+            var content = 
+            return content;
+        }
     }
 }
