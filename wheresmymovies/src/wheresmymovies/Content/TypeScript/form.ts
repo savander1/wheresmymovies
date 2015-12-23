d65a03e78fdbcd111094ea5f8371ecb5a5ebf8de
Shane Vander Meer <me@shanevandermeer.com>
2015-12-22 20:23:13 -0800

    Making form more generic

--- wheresmymovies/src/wheresmymovies/Content/TypeScript/form.ts
+++ wheresmymovies/src/wheresmymovies/Content/TypeScript/form.ts
@@ -14,7 +14,7 @@ module Form {
         'none'
     }
     
-    abstract class Field implements Renderable{
+    abstract class Field implements Renderable, Validatable{
         
         fieldType: FieldValidationType;
         label: string;
@@ -38,6 +38,24 @@ module Form {
             return void 0;
         }
         
+        isValid():boolean {
+            if (this.fieldType === FieldValidationType.none){
+                return true;
+            }
+            var elem = document.getElementById(this.id) as HTMLInputElement;
+            var value = elem.value;
+            
+            if (this.fieldType === FieldValidationType.text || this.fieldType === FieldValidationType.textbox){
+                return this.validateText(value)
+            }
+            
+            if (this.fieldType === FieldValidationType.number){
+                return this.validateNumber(value);
+            }
+            
+            throw 'Invalid FieldValidationType';
+        }
+        
         isPopulated(value: any): boolean{
             return value !== void 0 && value !== ''
         }
@@ -45,12 +63,19 @@ module Form {
         createElement(tagName:string): HTMLElement {
             return document.createElement(tagName);
         }
+        
+        private validateText(text:string):boolean{
+            return this.isPopulated(text);
+        }
+        
+        private validateNumber(text:any) : boolean{
+            return !isNaN(text);
+        }
     }
     
-    class TextField  extends Field implements Renderable, Validatable{
+    export class TextField  extends Field{
         
-        
-        public render(): Element{
+        render(): Element{
             var me = this;
             
             var containerElement = super.createElement('div');
@@ -88,21 +113,110 @@ module Form {
             
             return containerElement;
         }
-        
-        public isValid():boolean {
-            var elem = document.getElementById(this.id) as HTMLInputElement;
-            var value = elem.value;
+    }
+    
+    export class TextAreaField extends Field{
+        render(): Element{
+            var me = this;
             
-            return this.isPopulated(value);
+            var containerElement = super.createElement('div');
             
+            var labelElement = super.createElement('label')
+            labelElement.textContent = this.label;
+            
+            containerElement.appendChild(labelElement);
+            
+            var inputElement = super.createElement('textarea') as HTMLTextAreaElement
+            
+            if (this.isPopulated(this.name)){
+                inputElement.name = this.name;
+            }
+            
+            if (this.isPopulated(this.id)){
+                inputElement.id = this.id;
+            }
+            
+            if (this.isPopulated(this.cls)){
+                inputElement.className = this.cls;
+            }
+            
+            if (this.fieldType !== FieldValidationType.none){
+               inputElement.addEventListener('blur',  function(){
+                   if (!me.isValid()){
+                       inputElement.className += ' invalid';
+                   } else {
+                       inputElement.className = inputElement.className.replace(' invalid', '');
+                   }
+               });
+            }
+            
+            containerElement.appendChild(inputElement);
+            
+            return containerElement;
         }
     }
     
-    export class AddModvieForm {
+    export class Button implements Renderable{
+        
+        onclick:EventListenerObject;
+        buttonText:string;
+        
+        constructor(buttonText:string, onclick: EventListenerObject){
+            this.onclick = onclick;
+            this.buttonText = buttonText;
+        }
+        
+        render(): Element{
+            var me = this;
+            
+            var inputElement = document.createElement('button') as HTMLButtonElement
+            inputElement.addEventListener('click', onclick)
+            
+            return inputElement;
+        }
+    }
+    
+    export class RenderableForm implements Validatable{
+        fields: Field[];
+        buttons: Button[];
+        rootElement: HTMLElement;
+        
+        constructor(root:string, fields: Field[], buttons: Button[]){
+            this.fields = fields;
+            this.buttons = buttons;
+            this.rootElement = document.getElementById(root);
+        }
+        
+        render(name: string = null, id:string = null, method:string = null, action:string = null): void{
+            var formElement = document.createElement('form')
+            
+            formElement.method = method;
+            formElement.action = action;
+            formElement.name = name;
+            formElement.id = id;
+            
+            this.fields.forEach(element => {
+                formElement.appendChild(element.render());
+            });
+            
+            this.rootElement.appendChild(formElement);
+        }
+        
+        isValid() :boolean{
+            this.fields.forEach(element => {
+                if (element.isValid() !== true){
+                    return false;
+                }
+            });
+            return true;
+        }
+    }
+    
+    export class AddMovieForm {
         fields: Field[];
         rootElement: HTMLElement;
         
-        constructor(root:string){
+        constructor(root:string, fields: Field[]){
             this.fields = [
                 new TextField(FieldValidationType.text, 'Title', 'form', 'title')
             ];
@@ -124,6 +238,6 @@ module Form {
 }
 
 window.onload = function(){
-    var form = new Form.AddModvieForm('boo');
+    var form = new Form.AddMovieForm('boo', null);
     form.render();
 };
