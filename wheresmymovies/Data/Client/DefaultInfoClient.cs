f617b77ba248591e6266bca6556da78fb2d95e06
Shane Vander Meer <me@shanevandermeer.com>
2017-08-10 13:53:41 -0700

    Adding default client

---
+++ wheresmymovies/Data/Client/DefaultInfoClient.cs
@@ -0,0 +1,18 @@
+﻿// DELETE THIS CLASS ONCE WE HAVE A PROPER WAY TO GET METADATA
+using System.Threading.Tasks;
+using wheresmymovies.Entities;
+using wheresmymovies.Models;
+
+namespace wheresmymovies.Data.Client
+{
+    public class DefaultInfoClient : IInfoClient
+    {
+        public Task<Movie> GetMovieAsync(SearchParameters parameters)
+        {
+            return new Task<Movie>(() =>
+            {
+                return new Movie();
+            });
+        }
+    }
+}
