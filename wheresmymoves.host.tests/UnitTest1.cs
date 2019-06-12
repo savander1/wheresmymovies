ce61a62f450ed31c9c8a9280492a798b76803a1c
Shane Vander Meer <me@shanevandermeer.com>
2019-06-11 20:47:16 -0700

    Adding test projects

---
+++ wheresmymoves.host.tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace wheresmymoves.host.tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
